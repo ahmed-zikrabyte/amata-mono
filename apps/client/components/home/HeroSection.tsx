a6d476afd37ecc08865fc6cc5d41cd44b6fcd448
Vasudev290 <vasukesavulu@gmail.com>
2025-10-16 18:30:14 +0530

    feat: Product details Section page

---
+++ apps/client/components/home/HeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import carousel1 from "../../assets/carousel1.png"
+import carousel2 from "../../assets/carousel2.png"
+import carousel3 from "../../assets/carousel3.png"
+
+const HeroSection = () => {
+  return (
+    <div>HeroSection</div>
+  )
+}
+
+export default HeroSection
